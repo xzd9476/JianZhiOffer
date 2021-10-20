1eee3e7d04fc4bb23122b7a37cfa5018b2fd025b
xzd9476 <xzd9476@163.com>
2021-10-20 19:54:52 +0800

    test3

--- src/com/a/T包含min函数的栈.java
+++ src/com/a/T包含min函数的栈.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class T包含min函数的栈 {
 	/*
-	 * 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））。
+	 *test 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））。
 	 */
 	private Stack<Integer> stack1 = new Stack<Integer>();
 	private Stack<Integer> stack2 = new Stack<Integer>();
