1d67634e1169b8c4c1ee1020a8a652562fa92d6b
XZhidong <xzd9476@gmail.com>
2021-10-20 19:46:59 +0800

    test

--- src/com/a/Z二叉搜索树与双向链表.java
+++ src/com/a/Z二叉搜索树与双向链表.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 
 public class Z二叉搜索树与双向链表 {
 	/*
-	 * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。 要求不能创建任何新的结点，只能调整树中结点指针的指向。
+	 * test输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。 要求不能创建任何新的结点，只能调整树中结点指针的指向。
 	 */
 	public static class TreeNode {
 		int val = 0;
